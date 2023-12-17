e49bab358422fc7ed0af66adbba987112229774c
NathalieFeiner <63855353+NathalieFeiner@users.noreply.github.com>
2023-12-17 10:47:32 +0100

    Update 03_QC_and_Stats.sh

--- Bash_scripts/03_QC_and_Stats.sh
+++ Bash_scripts/03_QC_and_Stats.sh
@@ -5,16 +5,16 @@ module load BEDTools/2.29.2
 
 #obtaining the number of SNPs per 10kb-window before and after filtering:
 bedtools makewindows -g /crex/proj/snic2020-6-73/Projects/Wallie_Genomics/Genomes/Pmu_Spanish/GCA_004329235.1_PodMur_1.0_genomic.genomeFile.txt -w 10000 > windows.bed
-bedtools coverage -a windows.bed -b ../Filtering/GATK_all_SNP_final_new_cand20mb.recode.vcf -counts > New/GATK_all_SNP_filtered_pass_cand_plusminus20mb_coverage_10kb
-bedtools coverage -sorted -a windows.bed -b ../Filtering/GATK_all_SNP_final_new_prefiltered_cand20mb.vcf.recode.vcf -counts > New/GATK_all_SNP_filtered_cand_plusminus20mb_coverage_10kb
+bedtools coverage -a windows.bed -b ../Filtering/GATK_all_SNP_final_cand20mb.recode.vcf -counts > GATK_all_SNP_filtered_pass_cand_plusminus20mb_coverage_10kb
+bedtools coverage -sorted -a windows.bed -b ../Filtering/GATK_all_SNP_final_prefiltered_cand20mb.vcf.recode.vcf -counts > GATK_all_SNP_filtered_cand_plusminus20mb_coverage_10kb
 
 #obtaining the raw read depth (DP; pre-filtering) per sample per SNP:
-grep -v '#' ../Filtering/GATK_all_SNP_final_new_prefiltered_cand.vcf.recode.vcf | cut -f 10-159 | awk '{for(i=1;i<=NF;++i) cols[i]} {for(i=1;i<=NF;i++) if(i in cols) {split($i,t,":"); $i=t[3]}}1' > tmp1
-grep -v '##' ../Filtering/GATK_all_SNP_final_new_prefiltered_cand.vcf.recode.vcf | head -1 | cut -f 10-159 > IDs
+grep -v '#' ../Filtering/GATK_all_SNP_final_prefiltered_cand.vcf.recode.vcf | cut -f 10-159 | awk '{for(i=1;i<=NF;++i) cols[i]} {for(i=1;i<=NF;i++) if(i in cols) {split($i,t,":"); $i=t[3]}}1' > tmp1
+grep -v '##' ../Filtering/GATK_all_SNP_final_prefiltered_cand.vcf.recode.vcf | head -1 | cut -f 10-159 > IDs
 cat IDs tmp1 > tmp2
-grep -v '##' ../Filtering/GATK_all_SNP_final_new_prefiltered_cand.vcf.recode.vcf | cut -f 2 > pos
-paste pos tmp2 > New/Read_Depth_Per_Ind
-sed -i 's/ /\t/g' New/Read_Depth_Per_Ind
+grep -v '##' ../Filtering/GATK_all_SNP_final_prefiltered_cand.vcf.recode.vcf | cut -f 2 > pos
+paste pos tmp2 > Read_Depth_Per_Ind
+sed -i 's/ /\t/g' Read_Depth_Per_Ind
 rm tmp* IDs pos
 
 # the resulting files were imported into R and plotted with the R-script Coverage_stats.R
@@ -47,7 +47,7 @@ module load plink/1.90b4.9
 module load python/2.7.11
 
 #run plink for every chromosome (here chr. 1 is shown as example) and summarize decay for each chromosome using python script 'ld_decay_calc.py' (downloaded from github: https://github.com/speciationgenomics/scripts/blob/master/ld_decay_calc.py)
-plink --vcf ../Filtering/GATK_all_SNP_final_new.vcf -aec --set-missing-var-ids @:# --chr CM014743.1 --thin 0.1 -r2 gz --ld-window 100 --ld-window-kb 50 --ld-window-r2 0 --out LD_decay_chr1 --mind 0.5 --geno 0.5
+plink --vcf ../Filtering/GATK_all_SNP_final.vcf -aec --set-missing-var-ids @:# --chr CM014743.1 --thin 0.1 -r2 gz --ld-window 100 --ld-window-kb 50 --ld-window-r2 0 --out LD_decay_chr1 --mind 0.5 --geno 0.5
 python ld_decay_calc.py -i LD_decay_chr1.ld.gz -o LD_decay_chr1
 
 # the resulting files were imported into R and plotted with the R-script R_LD_decay_final.R
@@ -58,6 +58,6 @@ python ld_decay_calc.py -i LD_decay_chr1.ld.gz -o LD_decay_chr1
 module load bioinfo-tools
 module load plink/1.90b4.9
 
-plink --vcf ../Filtering/GATK_all_SNP_final_new_cand21_22.recode.vcf --allow-extra-chr --set-missing-var-ids @:# --r2 --ld-window 100000 --ld-window-kb 1000 --ld-window-r2 0 --out 1mb_pairwise_ld
+plink --vcf ../Filtering/GATK_all_SNP_final_cand21_22.recode.vcf --allow-extra-chr --set-missing-var-ids @:# --r2 --ld-window 100000 --ld-window-kb 1000 --ld-window-r2 0 --out 1mb_pairwise_ld
 
 # the resulting files were imported into R and plotted with the R-script LD_heatmap.R
